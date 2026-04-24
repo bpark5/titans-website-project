af5eae41c92513524dae0736c6f9ad72075dfd1a
Brandon Park <bdp5295@psu.edu>
2026-04-24 10:49:15 -0400

    Chris can't read

--- titans-website-project.js
+++ titans-website-project.js
@@ -101,7 +101,7 @@ export class TitansWebsiteProject extends DDDSuper(I18NMixin(LitElement)) {
     }
     return html`
     <div class="wrapper">
-        <titans-website-profile-picture username="Brandons Mamama"></titans-website-profile-picture>
+        <titans-website-profile-picture username="Random username"></titans-website-profile-picture>
         <titans-website-nav-bar 
           logoSrc="https://chatgpt.com/backend-api/estuary/public_content/enc/eyJpZCI6Im1fNjllNjM1MjI4Mjg4ODE5MTk3Y2IyYTg5ZGIyYjdmOWI6ZmlsZV8wMDAwMDAwMDk2OTA3MWY4YTZkYjU0MTU4YjJmMjk2OSIsInRzIjoiMjA1NjMiLCJwIjoicHlpIiwiY2lkIjoiMSIsInNpZyI6ImE4Yzc0MWNhMjQyNjM5NGI2YmYzNDc5NzZjZmY5MDgwZTI2ODgwOGU5MWY5MzZmMzJmNDA5OTFiYWJjODczNjIiLCJ2IjoiMCIsImdpem1vX2lkIjpudWxsLCJjcyI6bnVsbCwiY2RuIjpudWxsLCJjcCI6bnVsbCwibWEiOm51bGx9">
         </titans-website-nav-bar>
