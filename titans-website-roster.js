2868c35a252bde5264bd3acc11cd64d0d5d1a5aa
cjh6976-prog <cjh6976@psu.edu>
2026-04-30 19:07:34 -0400

    roster fixed

--- titans-website-roster.js
+++ titans-website-roster.js
@@ -14,9 +14,9 @@ export class TitansWebsiteRoster extends DDDSuper(I18NMixin(LitElement)) {
         super();
         this.players = [
             { name: "Billy Bob", position: "Short Stop", image: "./images/player-1.png"},
-            { name: "Max Smith", position: "Center Field", image: "./images/player-2.png"},
+            { name: "Alejandro Carrea-Diaz", position: "Center Field", image: "./images/player-2.png"},
             { name: "John Tester", position: "Pitcher", image: "./images/player-3.png"},
-            { name: "Sam Johnson", position: "Catcher", image: "./images/player-4.png"},
+            { name: "Brandon Park Jr.", position: "Catcher", image: "./images/player-4.png"},
 
         ];
 
@@ -61,7 +61,7 @@ export class TitansWebsiteRoster extends DDDSuper(I18NMixin(LitElement)) {
             }
             h2 {
                 font-size: var(--ddd-font-size-m);
-                margin-bottom: var(--ddd-spacing-6) 0 var(--ddd-spacing-4);
+                margin: var(--ddd-spacing-6) 0 var(--ddd-spacing-4);
                 color: light-dark(var(--ddd-theme-default-info), var(--ddd-theme-default-shrineLight));;
             }
             .roster-grid {
@@ -71,20 +71,23 @@ export class TitansWebsiteRoster extends DDDSuper(I18NMixin(LitElement)) {
                 gap: var(--ddd-spacing-5);
             }
             .person-card {
-                width: 180px;
+                width: 250px;
                 text-align: center;
             }
             .person-card img {
                 width: 100%;
                 display: block;
-                height: 180px;
-                object-fit: cover;
+                height: 250px;
+                object-fit: contain;
+                object-position: center top;
+                background: var(--ddd-theme-default-nittanyNavy);
                 border-radius: var(--ddd-radius-sm);
             }
             .name {
                 font-weight: 700;
                 font-size: var(--ddd-font-size-4xs);
                 margin-top: var(--ddd-spacing-2);
+                color: light-dark(var(--ddd-theme-default-info), var(--ddd-theme-default-shrineLight));;
             }
             .role {
                 font-size: var(--ddd-font-size-5);
@@ -119,13 +122,16 @@ export class TitansWebsiteRoster extends DDDSuper(I18NMixin(LitElement)) {
 
     render() {
         return html`
-            <div class="page">
-                <h1>Meet the Team</h1>
-                <div class="subtitle">Get to know the players and coaches who make up our roster.</div>
-                <h2>Players</h2>
-                <div class="roster-grid">
-                    ${this.players.map(player => this.renderPerson(player))}
+        <titans-website-nav-bar logoSrc="./images/Team-logo.png"></titans-website-nav-bar>
+         
+        <div class="page">
+            <h1>Meet the Team</h1>
+            <div class="subtitle">Get to know the players and coaches who make up our roster.</div>
+            <h2>Players</h2>
+            <div class="roster-grid">
+                ${this.players.map(player => this.renderPerson(player))}
                 </div>
+
                 <h2>Coaches</h2>
                 <div class="roster-grid">
                     ${this.coaches.map(coach => this.renderPerson(coach))}
